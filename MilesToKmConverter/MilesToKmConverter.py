f67a5d0b57e644daa7ea29894aea46ea036518c6
phil-qa <104486392+phil-qa@users.noreply.github.com>
2022-05-22 17:04:24 +0100

    implemented the code

---
+++ MilesToKmConverter/MilesToKmConverter.py
@@ -0,0 +1,9 @@
+#Convert the value in miles to Kilometers using the multiplier 1.6 * the value
+
+distance = input ('How many miles :')
+
+distance = int(distance)
+
+km = 1.6* distance
+
+print(f'The distance for {distance} as Km is {km}')
